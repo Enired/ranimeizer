1b559b829eeef25b7c28651a833f36491b7efa2e
Derine Banis <d.banis.13@gmail.com>
2022-08-08 19:04:29 -0700

    Initial setup of page with logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [anime, setAnime] = useState('')
+
+  useEffect(()=>{
+  },[anime])
+
+
+
+
   return (
     <div className="App">
       <h1>Welcome to Ranimeizer!!!!!</h1>
+      <span>You should watch: {anime}</span>
+      <br/>
       <button>Rewatch</button>
       <button>Something New!</button>
     </div>
