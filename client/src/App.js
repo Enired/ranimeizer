a480f10ee759bce8080c19610057791c327d0ae1
Derine Banis <d.banis.13@gmail.com>
2022-08-24 16:31:12 -0700

    Remove extra buttons. Now only one button thats a random anime. Comment out unused imports and functions.

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import './styles/button-style.scss'
 import { useState, useEffect } from 'react';
@@ -7,15 +7,15 @@ import axios from 'axios'
 import { Button } from './components/Button';
 
 //MUI ICONS
-import NewReleasesOutlinedIcon from '@mui/icons-material/NewReleasesOutlined';
-import ReplayOutlinedIcon from '@mui/icons-material/ReplayOutlined';
+// import NewReleasesOutlinedIcon from '@mui/icons-material/NewReleasesOutlined';
+// import ReplayOutlinedIcon from '@mui/icons-material/ReplayOutlined';
 
 function App() {
   const [anime, setAnime] = useState('')
 
-  const getRandomAnime = () => {
-    axios.get('/my_animes/random').then(res=>setAnime(res.data.name))
-  }
+  // const getRandomAnime = () => {
+  //   axios.get('/my_animes/random').then(res=>setAnime(res.data.name))
+  // }
 
   //Here we are testing getting animes from the anilist api
   const query= `
@@ -41,23 +41,30 @@ function App() {
   const getAni = () => {
     axios.post('https://graphql.anilist.co', {query, variables}, {headers})
     .then(res => {
-      console.log(res.data);
+      console.log(res);
       if(res.data.data.Media.title.english){
-        console.log(res.data.data.Media.title.english)
+        setAnime(res.data.data.Media.title.english)
       }
       else{
-        console.log(res.data.data.Media.title.romaji)
+        setAnime(res.data.data.Media.title.romaji)
       }
     })
     .then(variables = {id: Math.floor(Math.random() * 20000) + 1})
-    .catch(()=>getAni())
+    .catch((err)=>{
+      if(err.response.status === 404){
+        getAni()
+      }
+      else{
+        console.log(err.response.status);
+      } 
+    })
 
   }
 
   //End of test
   
   useEffect(()=>{
-    getRandomAnime();
+    getAni();
   },[])
 
   return (
@@ -65,8 +72,8 @@ function App() {
       <h1>Ranimeizer</h1>
       <span>You should watch: {anime}</span>
       <br/>
-      <Button text={'Rewatch'} onClick={getRandomAnime} />
-      <Button text={'Something New'} onClick={getRandomAnime}/>
+      {/* <Button text={'Rewatch'} onClick={getRandomAnime} />
+      <Button text={'Something New'} onClick={getRandomAnime}/> */}
       <Button text={'test'} onClick={getAni}/>
 
 
